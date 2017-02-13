e298ed582bfbfc0ae7a202350c8f51f852b883e8
Robin Hermansson <stealthfella@gmail.com>
2017-02-13 10:17:07 +0000

    gjorde om tutorialen

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,10 @@
 class ContactsController < ApplicationController
   def new
-    @contact = Contact.new
+    @contact = Contact.new  
   end
+  
   def create
+    
     @contact = Contact.new(contact_params)
     if @contact.save
       redirect_to new_contact_path, notice: "Message sent."
